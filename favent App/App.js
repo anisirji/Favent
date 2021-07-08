3eee19fe186287c64ed0b81de42296485f9718ab
Aniket Singh <www.allinoneforyouhere@gmail.com>
2021-07-08 15:06:28 +0530

    Login Page

--- favent App/App.js
+++ favent App/App.js
@@ -1,29 +1,70 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  TextInput,
+  Image,
+  ImageBackground,
+} from "react-native";
 import tailwind from "tailwind-rn";
+
+const Logo = require("./assets/logo.png"),
+  Bg = require("./assets/img.jpeg"),
+  Login = require("./assets/login.png");
 
 export default function App() {
   return (
-    <View style={tailwind(" m-5 mt-9")}>
-      <Text style={tailwind("text-red-500 text-4xl")}>Work done:- </Text>
-      <Text style={tailwind("ml-7 p-3")}>
-        1) Creating all environment for react-native app for Favent
+    <View>
+      <Image source={Login} style={tailwind("w-full h-full")} />
+      {/*<Image source={Bg} style={tailwind("absolute opacity-30 mr-72")} />
+      
+      <View style={tailwind("flex flex-col items-center")}>
+        <Image source={Logo} style={tailwind(" ml-10 mt-10 mr-10 ")} />
+        <Text style={tailwind("text-3xl text-blue-900 font-bold mt-3")}>
+          FAVENT
+        </Text>
+        <Text style={tailwind("text-lg text-blue-600")}>
+          Find Your Perfect Job
+        </Text>
+        <Text style={tailwind("text-4xl font-bold mt-5")}>Welcome Back !</Text>
+        <Text> Email Id</Text>
+        <TextInput
+          defaultValue="Email Id"
+          style={tailwind("border-2 rounded-md bg-white p-2 w-4/5")}
+        />
+        <Text>Password</Text>
+        <TextInput
+          defaultValue="Password"
+          style={tailwind("border-2 rounded-md bg-white p-2 w-4/5")}
+        />
+        <Text>Forget Passsword ?</Text>
+        <Button title="Login" style={style.login} />
+      </View>
+      <Text>
+        Or <Text>connect Using</Text>
       </Text>
-      <Text style={tailwind("ml-7 p-3")}>
-        2) Configuring important dependencies and libraries{" "}
+      <View style={tailwind("flex justify-around ")}>
+        <Button title="Google" />
+        <Button title="Facebook" />
+      </View>
+      <Text>
+        Don't have an account ? <Text>Sign up</Text>
       </Text>
-      <Text style={tailwind("ml-7 p-3")}>3) Configured Tailwind Css </Text>
-
-      <Text style={tailwind("text-red-500 text-4xl")}>Next release </Text>
-      <Text style={tailwind("ml-7 p-3")}>
-        1) Complete Login,Signup Forgot password page
-      </Text>
-      <Text style={tailwind("ml-7 p-3")}>
-        2) Work on Profile Creation Part{" "}
-      </Text>
-      <Text style={tailwind("ml-7 p-3")}>3)Start with Homepage </Text>
-      <StatusBar style="auto" />
+     */}
     </View>
   );
+}
+
+{
+  /*const style = StyleSheet.create({
+  login: {
+    padding: 10,
+    backgroundColor: "#0E477E",
+    color: "red",
+  },
+});*/
 }
